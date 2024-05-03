c685b859c22e2f980d5d924cd142af2554554b2a
Monkey King <45637966+luungockhang@users.noreply.github.com>
2024-05-03 10:19:43 +0700

    crawler file added

---
+++ crawler.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+# load website
+url = "https://www.lix.polytechnique.fr/~hermann/conf.php"
+
+res = requests.get(url)
+doc = BeautifulSoup(res.text, "html.parser")
+
+
+# business
+
+# find table with conference class
+tables = doc.find_all('table',class_="conference")
+
+# take all the rows from tables
+rows = tables.find_all('tr') 
+
+for line in rows:
+    cells = line.find_all('td')
+    for cell in cells:
+        print(cell.string)
