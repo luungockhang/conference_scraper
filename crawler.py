430e91808f0dac6703696d5a2ae7507308c6ffa4
Monkey King <45637966+luungockhang@users.noreply.github.com>
2024-05-14 14:25:27 +0700

    added pandas library to process csv

--- crawler.py
+++ crawler.py
@@ -2,22 +2,35 @@ from bs4 import BeautifulSoup
 import csv
 import requests
 
+
 # load website
 url = "https://www.lix.polytechnique.fr/~hermann/conf.php"
 res = requests.get(url)
 doc = BeautifulSoup(res.text, "html.parser")
 tables = doc.find_all('table')
 # Write data to CSV file
-with open('output.csv', 'w',encoding='utf-8', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    table_count = 1
-    for table in tables:
-        a = ['table '+ str(table_count)] 
-        writer.writerow(a)
+
+file_number = 1
+file_name = 'output' + str(file_number) + '.csv' 
+for table in tables:
+    with open(file_name, 'w',encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
         rows = table.find_all('tr')
         table_data = []
         for row in rows:
             cell_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
             table_data.append(cell_data)
         writer.writerows(table_data)
-        table_count +=1
+        file_number+=1
+        file_name = 'output' + str(file_number) + '.csv'
+
+        # for table in tables:
+        #     a = ['table '+ str(table_count)] 
+        #     writer.writerow(a)
+        #     rows = table.find_all('tr')
+        #     table_data = []
+        #     for row in rows:
+        #         cell_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
+        #         table_data.append(cell_data)
+        #     writer.writerows(table_data)
+        #     table_count +=1
