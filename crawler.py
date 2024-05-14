5d71fed7013fc0cd8c617841d852e3a267d3afaf
Monkey King <45637966+luungockhang@users.noreply.github.com>
2024-05-14 17:20:08 +0700

    added conf link crawl

--- crawler.py
+++ crawler.py
@@ -20,8 +20,20 @@ def crawler():
             rows = table.find_all('tr')
             table_data = []
             for row in rows:
-                cell_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
-                table_data.append(cell_data)
+                cell_with_confname = row.find_all("td",class_="confname")
+                # [<td class="confname"><a href="https://www.iclp24.utdallas.edu/" target="_blank">ICLP<br>&nbsp;</a>
+                #   <span class="tooltiptext">International Conference on Logic Programming</span>
+                # </td>]
+                if len(cell_with_confname) != 0: 
+                    a_tag = cell_with_confname[0].find_all("a",href=True) 
+                    # [<a href="https://www.iclp24.utdallas.edu/" target="_blank">ICLP<br>&nbsp;</a>]
+                    website_link = a_tag[0]['href']
+                    cell_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
+                    cell_data.append(website_link)
+                    table_data.append(cell_data)
+                else:
+                    cell_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
+                    table_data.append(cell_data)
             writer.writerows(table_data)
             file_number+=1
             file_name = 'output' + str(file_number) + '.csv'
